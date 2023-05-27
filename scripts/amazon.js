675eae0839568033ae3acd4349e1c95462b8de43
Gaurav Sonawane <vgauravs2001@gmail.com>
2023-05-27 15:13:08 +0530

    take quantity from dropdown instead of plain 1

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -66,12 +66,13 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         matchingItem = item;
       }
     });
+    let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
     let cartQuantity = 0;
